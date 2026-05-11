d2c46a2b0ae52ec8545414f617efffc9eb048ab0
unknown <rizkyyurdan26@gmail.com>
2026-05-11 15:44:09 +0700

    fix typo

--- src/components/ui/FooterUi.vue
+++ src/components/ui/FooterUi.vue
@@ -1,18 +1,23 @@
 <template>
-    <footer class="flex flex-col gap-2 items-center w-full border-t border-slate-300 p-4">
-
-            <h1>Need Collaboration?</h1>
-            <a href="" @click="handleEmail" target="_blank" class="flex gap-2  items-center text-xs text-blue-800 hover:text-blue-600"> <Icon icon="ic:outline-email"/> Send email </a href="">
-
-      
-
-    </footer>
+  <footer
+    class="flex flex-col gap-1 items-center w-full border-t border-slate-300 p-4"
+  >
+    <h1 class="text-secondary">How to reach me?</h1>
+    <h1 class="text-secondary">Need collaboration?</h1>
+    <a
+      href=""
+      @click="handleEmail"
+      target="_blank"
+      class="flex gap-2 items-center text-sm text-blue-800 hover:text-blue-600"
+    >
+      <Icon icon="ic:outline-email" /> Send email
+    </a>
+  </footer>
 </template>
 
 <script setup>
-import { Icon } from '@iconify/vue';
-import { useMail } from '../../compossable/useMail';
+import { Icon } from "@iconify/vue";
+import { useMail } from "../../compossable/useMail";
 
-const handleEmail = useMail
-
+const handleEmail = useMail;
 </script>
